27813e05e01dda53d4a0513bd03254d949031d13
BeokBeok <kekemusa37@nate.com>
2019-09-11 17:21:22 +0900

    Retrofit 헤더 설정

--- app/src/main/java/com/beok/daumimagesearch/data/remote/ImageSearchService.kt
+++ app/src/main/java/com/beok/daumimagesearch/data/remote/ImageSearchService.kt
@@ -3,10 +3,12 @@ package com.beok.daumimagesearch.data.remote
 import com.beok.daumimagesearch.data.model.ImageResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface ImageSearchService {
 
+    @Headers("Authorization: KakaoAK 7152b9bf3a3d93354032271f565d1cfe")
     @GET("v2/search/image")
     fun getImage(
         @Query("query")
