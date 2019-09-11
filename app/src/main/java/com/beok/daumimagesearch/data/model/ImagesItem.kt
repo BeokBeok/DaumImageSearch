1579b7341ccdc9c4b717303a2ef0e65207145bcb
BeokBeok <kekemusa37@nate.com>
2019-09-12 00:11:04 +0900

    이미지 가로 사이즈 화면 폭과 동일 및 원본 비율 유지

--- app/src/main/java/com/beok/daumimagesearch/data/model/ImagesItem.kt
+++ app/src/main/java/com/beok/daumimagesearch/data/model/ImagesItem.kt
@@ -5,11 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class ImagesItem(
 
     @SerializedName("image_url")
-    val imageUrl: String,
-
-    @SerializedName("width")
-    val width: Int,
-
-    @SerializedName("height")
-    val height: Int
+    val imageUrl: String
 )
--- app/src/main/java/com/beok/daumimagesearch/ext/RecyclerViewExt.kt
+++ app/src/main/java/com/beok/daumimagesearch/ext/RecyclerViewExt.kt
@@ -3,11 +3,10 @@ package com.beok.daumimagesearch.ext
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.beok.daumimagesearch.base.BaseRecyclerView
-import com.beok.daumimagesearch.data.model.ImagesItem
 
 @Suppress("UNCHECKED_CAST")
 @BindingAdapter("replaceItem")
-fun RecyclerView.replaceItem(item: List<ImagesItem>?) {
+fun RecyclerView.replaceItem(item: List<Any>?) {
     (this.adapter as? BaseRecyclerView.Adapter<Any, *>)?.run {
         replaceItem(item)
         notifyDataSetChanged()
